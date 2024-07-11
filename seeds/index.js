b951ad16fbd0df2ef3e5a7e3c40143b17c9c5426
Wild Tapias <tapiaswilman@gmail.com>
2024-07-11 10:30:59 -0500

    add navbar, footer and images

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'This is test of description, is possible that is a bad idea but i dont know, i want try hahah',
+            price
         })
         await camp.save();
     }
